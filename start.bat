0b8f4a5bb5a3aeb7f23b9a0f5ef471e4c29e62e1
rubinov2016 <lrubinov@yandex.ru>
2025-06-11 20:28:59 +0100

    first commit

---
+++ start.bat
@@ -0,0 +1,7 @@
+echo "# Research" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:rubinov2016/Research.git
+git push -u origin main
